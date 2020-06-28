8356800e3ad4d3d594c97634ead7e6e7bb682bc0
ThaysMrtns <thays.martins@nave.org.br>
2020-06-28 15:30:57 -0300

    Otimização do código

--- calculadora.js
+++ calculadora.js
@@ -4,89 +4,73 @@ let calculadora = {};
 calculadora.display = document.querySelector(".display"); 
 
 //Referenciando botões
-let botaoZero = document.querySelector("#numeroZero");
-let botaoUm = document.querySelector("#numeroUm");
-let botaoDois = document.querySelector("#numeroDois");
-let botaoTres = document.querySelector("#numeroTres");
-let botaoQuatro = document.querySelector("#numeroQuatro");
-let botaoCinco = document.querySelector("#numeroCinco");
-let botaoSeis = document.querySelector("#numeroSeis");
-let botaoSete = document.querySelector("#numeroSete");
-let botaoOito = document.querySelector("#numeroOito");
-let botaoNove = document.querySelector("#numeroNove");
-let botaoSoma = document.querySelector("#soma");
-let botaoIgual = document.querySelector("#igual");
-let botaoMultiplica = document.querySelector("#multiplicar");
-let botaoDividi = document.querySelector("#dividir");
-let botaoSubtrai = document.querySelector("#subtrair");
-let botaoC = document.querySelector("#c");
+let botao = document.querySelectorAll(".btn-num");
+let botaoSinais = document.querySelectorAll(".btn-sinais");
 
 //Variavel que acumula expressões
 let acumulador = "";
 let resultado;
 
-
-//Evento de click no botãoUm
-botaoZero.onclick = function(){
+botao[10].onclick = function(){ //Botão 0
     calculadora.display.innerText +=0;
 }
-botaoUm.onclick = function() {
-    calculadora.display.innerText += 1; 
+botao[0].onclick = function(){  //Botão 1
+    calculadora.display.innerText +=1;
 }
-botaoDois.onclick = function(){
-    calculadora.display.innerText += 2;
+botao[1].onclick = function() { //Botão 2
+    calculadora.display.innerText += 2; 
 }
-botaoTres.onclick = function(){
+botao[2].onclick = function(){ //Botão 3
     calculadora.display.innerText += 3;
 }
-botaoQuatro.onclick = function(){
-    calculadora.display.innerText +=4;
+botao[3].onclick = function(){ //Botão 4
+    calculadora.display.innerText += 4;
 }
-botaoCinco.onclick = function(){
+botao[4].onclick = function(){ //Botão 5
     calculadora.display.innerText +=5;
 }
-botaoSeis.onclick = function(){
+botao[5].onclick = function(){ //Botão 6
     calculadora.display.innerText +=6;
 }
-botaoSete.onclick = function(){
-    calculadora.display.innerText += 7;
+botao[6].onclick = function(){ //Botão 7
+    calculadora.display.innerText +=7;
 }
-botaoOito.onclick = function(){  
-    calculadora.display.innerText +=8;
+botao[7].onclick = function(){ //Botão 8
+    calculadora.display.innerText += 8;
 }
-botaoNove.onclick = function(){
+botao[8].onclick = function(){   //Botão 9
     calculadora.display.innerText +=9;
 }
 
+
 //Subtrair
-botaoSubtrai.onclick = function(){
+botaoSinais[2].onclick = function(){
     acumulador += calculadora.display.innerText; //Guardando dentro do acumulador o valor escrito no display
     acumulador += " - "; //Guardando dentro do acumulador o -
     calculadora.display.innerText = ""; //Limpando o display
 }
 //Dividir
-botaoDividi.onclick = function(){
+botaoSinais[1].onclick = function(){
     acumulador += calculadora.display.innerText; //Guardando dentro do acumulador o valor escrito no display
     acumulador += " / "; //Guardando dentro do acumulador o /
     calculadora.display.innerText = ""; //Limpando o display
 }
 //Multiplicar
-botaoMultiplica.onclick = function(){
+botaoSinais[0].onclick = function(){
     acumulador += calculadora.display.innerText; //Guardando dentro do acumulador o valor escrito no display
     acumulador += " * "; //Guardando dentro do acumulador o *
     calculadora.display.innerText = ""; //Limpando o display
 }
 
 //Somar
-botaoSoma.onclick = function() {
+botaoSinais[3].onclick = function() {
     acumulador += calculadora.display.innerText; //Guardando dentro do acumulador o valor escrito no display
     acumulador += " + "; //Guardando dentro do acumulador o +
     calculadora.display.innerText = ""; //Limpando o display
  
 }
-//Evento do botão igual
-botaoIgual.onclick = function() {
-    //TODO: checar se existe conteúdo dentro do display. pelo visto eu tenho que criar um contador né 
+//Iigual
+botaoSinais[4].onclick = function() {
     //colocando conteúdo do display no acumulador
     acumulador += calculadora.display.innerText; 
     //resolvendo conta (conteúdo acumulador)
@@ -97,10 +81,14 @@ botaoIgual.onclick = function() {
     // limpando o acumulador
     acumulador = "";
 }
-//C limpar
-botaoC.onclick = function(){
+//Limpar C
+botao[12].onclick = function(){
     //colocando no display o conteúdo do acumulador
     calculadora.display.innerText = "";
     // limpando o acumulador
     acumulador = "";
+}
+//Alterar sinal do número
+botao[9].onclick = function(){
+    console.log("clickado");
 }
